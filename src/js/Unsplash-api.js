d72b6dcca6f61f2aa616b26d4459e4fc527e2c6e
Natalia <holovshyk@gmail.com>
2025-02-22 12:27:42 +0200

    )

--- src/js/Unsplash-api.js
+++ src/js/Unsplash-api.js
@@ -3,6 +3,7 @@ import axios from "axios"
 export class UnsplashAPI {
     #BASE_URL = 'https://api.unsplash.com/search/photos'
     #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58'
+    #query = '';
 
     async getPopularPhotos(page) {
         const url = `${this.#BASE_URL}?page=${page}&per_page=12&query=popular&orientation=portrait&client_id=${this.#API_KEY}`
@@ -14,4 +15,20 @@ export class UnsplashAPI {
             console.log(error) 
         }
     }
+
+    async getPhotosByQuery(page) {
+        const url = `${this.#BASE_URL}?page=${page}&per_page=12&query=${this.#query}&orientation=portrait&client_id=${this.#API_KEY}`
+
+        try {
+            const response = await axios(url);
+            return response.data;
+        } catch (error) {
+            console.log(error) 
+        }
+    }
+
+    set query(newQuery){
+        this.#query = newQuery;
+    }
+    
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -10,12 +10,15 @@
 // Додай лоадер під час завантаження даних з бекенда.
 // Не забудь про відповідні перевірки і сповіщення при роботі з запитами і з формою.
 
+import iziToast from 'izitoast';
 import { UnsplashAPI } from './Unsplash-api';
 import { createGalleryCard } from './createGalleryCard';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 
 const gallery = document.querySelector('.gallery');
+const form = document.querySelector('.search-form');
+const input = document.querySelector('[name="query"]');
 
 const container = document.getElementById('tui-pagination-container');
 const pagination = new Pagination(container, {
@@ -35,13 +38,55 @@ api.getPopularPhotos(page).then(response => {
   console.log(response);
 });
 
-pagination.on('afterMove', event => {
+function getPopular(event) {
   const currentPage = event.page;
   api.getPopularPhotos(currentPage).then(response => {
   const markup = createGalleryCard(response.results);
   gallery.innerHTML = markup;
   console.log(response);
 });
-});
+}
+function getByQuery(e){
+  const currentPage = e.page;
+  api.getPhotosByQuery(currentPage).then(response => {
+  const markup = createGalleryCard(response.results);
+  gallery.innerHTML = markup;
+  console.log(response);
+})
+}
 
 
+pagination.on('afterMove', getPopular);
+
+form.addEventListener('submit', async (e)=>{
+  e.preventDefault();
+  const searchQuery = input.value.trim();
+  if(searchQuery === ''){
+    iziToast.info({
+      message: 'Enter something for search',
+    })
+    return;
+  }
+
+  api.query = searchQuery;
+  pagination.off('afterMove', getPopular);
+  pagination.off('afterMove', getByQuery);
+  try{
+    const data = await api.getPhotosByQuery(page);
+    if(data.results.length === 0){
+      iziToast.warning({
+        message:'Sorry, there are no images matching your search query. Please try again!'
+      })
+      return;
+    } 
+    pagination.on('afterMove', getByQuery);
+    const markup = createGalleryCard(data.results);
+    gallery.innerHTML = markup;
+
+    pagination.reset(data.total);
+
+  } catch(error){
+  console.log(error);
+  }
+
+})
