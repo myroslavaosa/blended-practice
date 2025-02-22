46cadb5086040413f8ae9ef685487519ce99fa9a
Oleksii <o.pruzhanskiy@gmail.com>
2025-02-22 10:48:43 +0200

    commit

---
+++ src/js/Unsplash-api.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export class UnsplashAPI {
+    #BASE_URL = 'https://api.unsplash.com/search/photos'
+    #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58'
+
+    async getPopulalPhotos(page) {
+        const url = `${this.#BASE_URL}?page=${page}&per_page=12&query=popular&orientation=portrait&client_id=${this.#API_KEY}`
+
+        try {
+            const response = await axios(url);
+            return response.data;
+        } catch (error) {
+            console.log(error) 
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -15,62 +15,63 @@
 // та недоступні для зміни поля з даними користувача.
 // Клік по кнопці logout повертає все до початкового вигляду і видаляє дані користувача
 // з локального сховища.
-import './js/modul-10';
-import icon from './img/javascript.svg';
-import 'izitoast/dist/css/iziToast.min.css';
-import iziToast from 'izitoast';
-import { refs } from './js/refs';
-import { get, remove, save } from './js/storage';
+// import './js/modul-10';
+// import icon from './img/javascript.svg';
+// import 'izitoast/dist/css/iziToast.min.css';
+// import iziToast from 'izitoast';
+// import { refs } from './js/refs';
+// import { get, remove, save } from './js/storage';
 
-const USER_DATA = {
-  email: 'user@mail.com',
-  password: 'secret',
-};
+// const USER_DATA = {
+//   email: 'user@mail.com',
+//   password: 'secret',
+// };
 
-const LS_KEY = 'userdata';
+// const LS_KEY = 'userdata';
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  if (refs.btn.textContent === 'Logout') {
-    remove(LS_KEY);
-    refs.form.reset();
-    refs.emailInfo.removeAttribute('readonly');
-    refs.passwordInfo.removeAttribute('readonly');
-    refs.btn.textContent = 'Login';
-    return;
-  }
-  const emailInput = refs.emailInfo.value.trim();
-  const passwordInput = refs.passwordInfo.value.trim();
-  if (emailInput === '' || passwordInput === '') {
-    iziToast.warning({
-      iconUrl: icon,
-      title: 'Caution',
-      message: 'Pls put something it!',
-    });
-    return;
-  }
-  if (emailInput !== USER_DATA.email || passwordInput !== USER_DATA.password) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Wrong password or email!',
-    });
-    return;
-  }
-  save(LS_KEY, {
-    email: emailInput,
-    password: passwordInput,
-  });
-  refs.btn.textContent = 'Logout';
-  refs.emailInfo.setAttribute('readonly', true);
-  refs.passwordInfo.setAttribute('readonly', true);
-});
+// refs.form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   if (refs.btn.textContent === 'Logout') {
+//     remove(LS_KEY);
+//     refs.form.reset();
+//     refs.emailInfo.removeAttribute('readonly');
+//     refs.passwordInfo.removeAttribute('readonly');
+//     refs.btn.textContent = 'Login';
+//     return;
+//   }
+//   const emailInput = refs.emailInfo.value.trim();
+//   const passwordInput = refs.passwordInfo.value.trim();
+//   if (emailInput === '' || passwordInput === '') {
+//     iziToast.warning({
+//       iconUrl: icon,
+//       title: 'Caution',
+//       message: 'Pls put something it!',
+//     });
+//     return;
+//   }
+//   if (emailInput !== USER_DATA.email || passwordInput !== USER_DATA.password) {
+//     iziToast.error({
+//       title: 'Error',
+//       message: 'Wrong password or email!',
+//     });
+//     return;
+//   }
+//   save(LS_KEY, {
+//     email: emailInput,
+//     password: passwordInput,
+//   });
+//   refs.btn.textContent = 'Logout';
+//   refs.emailInfo.setAttribute('readonly', true);
+//   refs.passwordInfo.setAttribute('readonly', true);
+// });
 
-const parseData = get(LS_KEY);
-if (parseData) {
-  refs.emailInfo.value = parseData.email || '';
-  refs.passwordInfo.value = parseData.password || '';
+// const parseData = get(LS_KEY);
+// if (parseData) {
+//   refs.emailInfo.value = parseData.email || '';
+//   refs.passwordInfo.value = parseData.password || '';
 
-  refs.emailInfo.setAttribute('readonly', true);
-  refs.passwordInfo.setAttribute('readonly', true);
-  refs.btn.textContent = 'Logout';
-}
+//   refs.emailInfo.setAttribute('readonly', true);
+//   refs.passwordInfo.setAttribute('readonly', true);
+//   refs.btn.textContent = 'Logout';
+// }
+
